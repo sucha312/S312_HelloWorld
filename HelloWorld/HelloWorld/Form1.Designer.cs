27d225a651c2f926b8c7e05c4e04d92c36d9bbcd
sucha312 <64030312@kmitl.ac.th>
2022-11-09 14:01:22 +0700

    Edit by dev1

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // label1
@@ -37,10 +40,35 @@
             this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.Location = new System.Drawing.Point(50, 59);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(159, 31);
+            this.label1.Size = new System.Drawing.Size(152, 31);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Branch : Main ";
+            this.label1.Text = "Branch : Dev1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(50, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(271, 145);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "กดๆ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(50, 312);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(250, 27);
+            this.dateTimePicker1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(425, 59);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 104);
+            this.listBox1.TabIndex = 3;
             // 
             // Form1
             // 
@@ -48,6 +76,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,5 +90,8 @@
         #endregion
 
         private Label label1;
+        private Button button1;
+        private DateTimePicker dateTimePicker1;
+        private ListBox listBox1;
     }
 }
--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -11,5 +11,10 @@ namespace HelloWorld
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
